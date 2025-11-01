c268249feea3f01cfc5bd07c62131c39d01c3a46
firdawardina <firdawardina02@gmail.com>
2025-11-01 15:35:36 +0700

    Add web routes for home, about, and contact pages

--- web.php
+++ web.php
@@ -1,1 +1,27 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('beranda');
+});
+
+Route::get('/beranda', [PageController::class, 'index'])->name('beranda');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/contact',[PageController::class, 'contact'])->name('contact'); 
+
+
 
